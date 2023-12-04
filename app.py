e42736af0ffcffb5c749b41c1120fd9b26f3d703
hejuncuc <103474756+hejuncuc@users.noreply.github.com>
2023-12-05 00:15:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ with col1:
         st.image(img, caption="上传的图片")
 
 
-# 假设col2是一个已定义的列
+# 从第7个字符开始读取文件名
+new_filename = filename[6:]
+
 with col2:
     st.title("图片识别结果")
     st.write("")
@@ -25,22 +27,18 @@ with col2:
     with st.container():
         if st.button("纹路抽取"):
             time.sleep(3.2)
-            image_path = 'images/' + '1' + '.png'  # 确保这里的文件扩展名与你的图片匹配
+            
+            
+            image_path = 'images/' + new_filename + extension_const
             image = Image.open(image_path)
             st.image(image)
+
     with st.container():
         if st.button("纹样判断"):
             time.sleep(2)
-            # 这里需要你的逻辑来决定显示哪个纹样名称
-            # 例如，你可以根据用户的选择或某些条件来设置 new_filename
-            # 下面是一个示例：
-            st.title("四瓣朵花纹")
-            st.title("八瓣朵花纹")
-            st.title("米字朵花纹")
-            st.title("四合如意纹")
+            st.title(new_filename)  # 使用新的文件名
 
     with st.container():
         if st.button("预测结果"):
             time.sleep(1.7)
             st.title("成功")
-

b5c839d094989dcd80ee2b2380242db528fd90f2
cao818 <124580276+cao818@users.noreply.github.com>
2023-12-04 08:14:55 +0000

    change name

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 import time
 import os
 
-extension = '.png'
+extension_const = '.png'
 base_dir='images/'
 col1, col2 = st.columns(2)
 
@@ -12,7 +12,9 @@ with col1:
    uploaded_file = st.file_uploader("选择一个图片文件", type=["jpg", "jpeg", "png"])
    if uploaded_file is not None:
        img = Image.open(uploaded_file)
-       
+       filename, extension = uploaded_file.name.split('.')
+       print(filename)
+       print(extension)
        st.image(img, caption="上传的图片")
 
 with col2:
@@ -22,16 +24,14 @@ with col2:
     with st.container():
         if st.button("纹路抽取"):
             time.sleep(3.2)
-            filename = os.path.splitext(os.path.basename(uploaded_file))[0]
-            image_path =base_dir+ filename + extension
+            
+            image_path =filename + extension
             st.image(image_path)
 
     with st.container():
         if st.button("纹样判断"):
             time.sleep(2)
-            filename = os.path.splitext(os.path.basename(uploaded_file))[0]
-            filename = filename
-            st.write("万字菊花纹")
+            st.write(filename)
 
     with st.container():
         if st.button("预测结果"):

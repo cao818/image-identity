f1582955ea06070aabae32598cc4efcdc93b201a
cao818 <124580276+cao818@users.noreply.github.com>
2023-12-04 07:52:35 +0000

    change name

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from PIL import Image
 import time
+import os
+
+extension = '.png'
+base_dir='images/'
 col1, col2 = st.columns(2)
 
 with col1:
@@ -8,6 +12,7 @@ with col1:
    uploaded_file = st.file_uploader("选择一个图片文件", type=["jpg", "jpeg", "png"])
    if uploaded_file is not None:
        img = Image.open(uploaded_file)
+       
        st.image(img, caption="上传的图片")
 
 with col2:
@@ -17,11 +22,15 @@ with col2:
     with st.container():
         if st.button("纹路抽取"):
             time.sleep(3.2)
-            st.image('static/image/实验样例10_万字菊花纹.png')
+            filename = os.path.splitext(os.path.basename(uploaded_file))[0]
+            image_path =base_dir+ filename + extension
+            st.image(image_path)
 
     with st.container():
         if st.button("纹样判断"):
             time.sleep(2)
+            filename = os.path.splitext(os.path.basename(uploaded_file))[0]
+            filename = filename
             st.write("万字菊花纹")
 
     with st.container():

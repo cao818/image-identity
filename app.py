ab6ba6ebed7a3cff17ffd0474fdda0e99bcdfd9c
hejuncuc <103474756+hejuncuc@users.noreply.github.com>
2023-12-04 20:57:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from PIL import Image
 import time
 import os
 
-filename=''
+filename = ''
 extension_const = '.png'
-base_dir='images/'
+base_dir = 'images/'
 col1, col2 = st.columns(2)
 
 with col1:
-   st.title("图片上传与显示")
-   uploaded_file = st.file_uploader("选择一个图片文件", type=["jpg", "jpeg", "png"])
-   if uploaded_file is not None:
-       img = Image.open(uploaded_file)
-       filename, extension = uploaded_file.name.split('.')  
-       st.image(img, caption="上传的图片")
+    st.title("图片上传与显示")
+    uploaded_file = st.file_uploader("选择一个图片文件", type=["jpg", "jpeg", "png"])
+    if uploaded_file is not None:
+        img = Image.open(uploaded_file)
+        filename, extension = uploaded_file.name.split('.')
+        st.image(img, caption="上传的图片")
 
 with col2:
     st.title("图片识别结果")
@@ -24,22 +24,18 @@ with col2:
         if st.button("纹路抽取"):
             time.sleep(3.2)
             
-            filename, extension = uploaded_file.name.split('.')
-            image_path ='images/'+filename + extension_const
+            # 从第7个字符开始读取文件名
+            new_filename = filename[6:]
+            image_path = 'images/' + new_filename + extension_const
             image = Image.open(image_path)
             st.image(image)
 
     with st.container():
         if st.button("纹样判断"):
             time.sleep(2)
-            st.title(filename)
+            st.title(new_filename)  # 使用新的文件名
 
     with st.container():
         if st.button("预测结果"):
             time.sleep(1.7)
             st.title("成功")
-
-
-
-
-
